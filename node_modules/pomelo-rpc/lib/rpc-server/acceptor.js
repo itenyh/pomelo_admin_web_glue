9ad219db53b75e857631432f2c955a3c961742c3
itenyh <itenyh@gmail.com>
2017-11-13 15:43:09 +0800

    update

--- node_modules/pomelo-rpc/lib/rpc-server/acceptor.js
+++
@@ -1,6 +0,0 @@
-var acceptor = require('./acceptors/mqtt-acceptor');
-// var acceptor = require('./acceptors/ws2-acceptor');
-
-module.exports.create = function(opts, cb) {
-	return acceptor.create(opts, cb);
-};
